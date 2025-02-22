47155167777bb20135c63ab9d0aa5c4df5b07632
Vivek Singh Bora <boravivek459@gmail.com>
2025-02-22 10:59:29 +0530

    Update EvenOrOdd.java

--- Math/EvenOrOdd.java
+++ Math/EvenOrOdd.java
@@ -1,13 +1,1 @@
-import java.util.Scanner;
-public class EvenOrOdd {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-    int a=sc.nextInt();
-    if(a%2==0){
-        System.out.println("number is even ");
-    }
-    else{
-        System.out.println("number is odd");
-    }
-    }
-}
+
